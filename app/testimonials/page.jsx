a62817b2898d02f737deadd40e2f67ad5c5ce1a6
Haider Ali <hassanhussaian630@gmail.com>
2023-08-31 23:14:29 -0700

    commit

--- app/testimonials/page.jsx
+++ app/testimonials/page.jsx
@@ -1,14 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
-
+"use client";
+import React from "react";
+import TestmonialSlider from "../../components/TestimonialSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../varaints";
 
 const page = () => {
   return (
-    <div>
-      
-      <Image src={'/thumb1.jpg'} width={500} height={300} alt='' />
+    <div className=" flex h-full  text-center py-32 bg-primary/30">
+      <div className="container mx-auto flex flex-col justify-center">
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2"
+        >
+          What clients <span className="text-accent">say.</span>
+        </motion.h2>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestmonialSlider />
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
